48fb20d69bbd615b78ed9d5c1fd62a0d72936b12
mehta-shreyas <spm7va@virginia.edu>
2021-08-09 14:41:15 -0400

    IMDB rating now formatted on title information screen

--- content.js
+++ content.js
@@ -1,38 +1,109 @@
-
 //Attempt to get selected title card
-var title= document.querySelector(".previewModal--player-titleTreatment-logo");
-if(title!=null){ //if a title is selected, perform actions
-    console.log(title.alt);
+var rawtitle= document.querySelector(".previewModal--player-titleTreatment-logo");
+if(rawtitle!=null){ //if a title is selected, perform actions
+    //console.log(rawtitle.alt);
+    let title= rawtitle.alt;
+    title=title.replace(/ /g, "+");
+    console.log(title);
+    const Url= "https://www.omdbapi.com/?t="+title+"&apikey=1867bf1d";
+    console.log(Url);
+    var omdbResults;
+    fetch(Url)
+    .then(data=>{
+        return data.json();
+    })
+    .then(res=>{
+        omdbResults=res;
+        console.log(omdbResults);
+    });
+    //omdbResults={"Title":"Death Note","Year":"2017","Rated":"TV-MA","Released":"25 Aug 2017","Runtime":"101 min","Genre":"Action, Crime, Drama","Director":"Adam Wingard","Writer":"Charley Parlapanides, Vlas Parlapanides, Jeremy Slater","Actors":"Nat Wolff, LaKeith Stanfield, Margaret Qualley","Plot":"A high school student named Light Turner discovers a mysterious notebook that has the power to kill anyone whose name is written within its pages, and launches a secret crusade to rid the world of criminals.","Language":"English, Japanese, Spanish","Country":"United States","Awards":"N/A","Poster":"https://m.media-amazon.com/images/M/MV5BMTUwOTgzMTEyOF5BMl5BanBnXkFtZTgwNTk3MTM5MjI@._V1_SX300.jpg","Ratings":[{"Source":"Internet Movie Database","Value":"4.4/10"},{"Source":"Rotten Tomatoes","Value":"38%"},{"Source":"Metacritic","Value":"43/100"}],"Metascore":"43","imdbRating":"4.4","imdbVotes":"79,564","imdbID":"tt1241317","Type":"movie","DVD":"25 Aug 2017","BoxOffice":"N/A","Production":"N/A","Website":"N/A","Response":"True"};
+
+    //Wait for api results to return before accessing values
+    setTimeout(() => {
+        console.log(omdbResults);
+        //Check results and isolate data if movie found in db
+        var titleNotFound=(omdbResults.Response=="False");
+        if(titleNotFound==false){
+            var ratings= omdbResults.Ratings;
+            var imdbVotes=omdbResults.imdbVotes;
+            var metaValue;
+            var imdbValue;
+            var rottenValue;
+
+            for(i=0; i<ratings.length; i++){
+                let current= ratings[i];
+                if (current.Source=="Metacritic")
+                    console.log("Metacritic Score: ", current.Value);
+                    metaValue="Metacritic Score: "+current.Value;
+
+                if (current.Source=="Internet Movie Database")
+                    //console.log("IMDB Score: ", current.Value, " (based on", imdbVotes, " votes)");
+                    //imdbValue="IMDB Score: "+current.Value;
+                    imdbValue=current.Value;
+                if (current.Source=="Rotten Tomatoes")
+                    //console.log("IRotten Tomatoes Score: ", current.Value);
+                    rottenValue="Rotten Tomatoes Score: "+current.Value;
+            }
+        }
+
+        //Create new div to hold IMDB rating content
+        var ratingDiv = document.createElement("div");
+        ratingDiv.style.class="wrapper";
+        ratingDiv.setAttribute("style", "text-align:center");
+        ratingDiv.setAttribute("style", "float:right");
+
+        //Create IMDB large text node
+        var textnodeLarge = document.createElement("span");
+        textnodeLarge.setAttribute("style", "font-size:20px");
+
+
+        //Create IMDB small text node
+        var textnodeSmall = document.createElement("span");
+        textnodeSmall.style.color="grey";
+        textnodeSmall.style.setProperty("font-style", "italic")
+        textnodeSmall.style.setProperty("font-size", "10px")
+
+        //IMDB icon formatting
+        var img = document.createElement("img");
+        img.style.class="icon";
+        img.src = "https://iconarchive.com/download/i80460/uiconstock/socialmedia/IMDb.ico";
+        img.style.width="80px";
+
+        //New div for rating text
+        var textDiv = document.createElement("div");
+        textDiv.style.setProperty("text-align", "center")
+        textDiv.style.setProperty("padding-top", "20px")
+        textDiv.style.setProperty("padding-right", "30px")
+        textDiv.id="textNodeSmall";
+
+        //New div for IMDB image
+        var imgDiv = document.createElement("div");
+        imgDiv.style.setProperty("text-align", "center");
+        imgDiv.style.setProperty("float", "left");
+
+        //Set text for rating
+        if(titleNotFound==true){
+            textnodeLarge.innerHTML="&nbsp&nbsp"+"N/A";
+            textnodeSmall.innerHTML="";
+            textDiv.style.setProperty("padding-top", "30px")
+        }else{
+            textnodeLarge.innerHTML="&nbsp"+imdbValue;
+            textnodeSmall.innerHTML="</br>"+"("+imdbVotes+" votes)";
+        }
+
+        //Append nodes to divs
+        textDiv.appendChild(textnodeLarge);
+        textDiv.appendChild(textnodeSmall);
+        imgDiv.appendChild(img);
+        ratingDiv.appendChild(imgDiv);
+        ratingDiv.appendChild(textDiv);
+
+        //Add main div to right-side info for selected title
+        document.querySelector(".previewModal--detailsMetadata-right").appendChild(ratingDiv);
+
+        console.log(document.querySelector(".previewModal--detailsMetadata-right"));
+        console.log(document.querySelector(".detail-modal-container").childNodes[1]);
+
+}, 1000);
+
 }
-
-
-
-
-
-
-
-// alert('Netflix');
-// var titles = document.getElementsByClassName("bob-title");
-// console.log("Title 1: ", titles[0])
-
-
-
-// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-//   if (request.message === 'TabUpdated') {
-//     console.log("HELLO");
-//   }
-// })
-
-// var titles= document.querySelectorAll(".fallback-text");
-// for(i=0; i<titles.length; i++){
-//   console.log("Title ", i, " : ", titles[i].textContent);
-// }
-
-//
-// chrome.runtime.onMessage.addListener(
-//   function(request, sender, sendResponse) {
-//     if( request.message == "clicked_browser_action" ) {
-//       console.log("HELLO")
-//     }
-//   }
-// );
