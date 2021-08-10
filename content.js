5df64a025b0b64dc840499a34313b417c6522b1c
mehta-shreyas <spm7va@virginia.edu>
2021-08-09 23:26:18 -0400

    Fixes and working links for imdb rating

--- content.js
+++ content.js
@@ -1,10 +1,12 @@
 //Attempt to get selected title card
 var rawtitle= document.querySelector(".previewModal--player-titleTreatment-logo");
 if(rawtitle!=null){ //if a title is selected, perform actions
+    var titleyear= document.querySelector(".videoMetadata--second-line").childNodes[0].innerHTML;
     //console.log(rawtitle.alt);
     let title= rawtitle.alt;
     title=title.replace(/ /g, "+");
     console.log(title);
+    //const Url= "https://www.omdbapi.com/?t="+title+"&y="+titleyear+"&apikey=1867bf1d";
     const Url= "https://www.omdbapi.com/?t="+title+"&apikey=1867bf1d";
     console.log(Url);
     var omdbResults;
@@ -16,7 +18,7 @@ if(rawtitle!=null){ //if a title is selected, perform actions
         omdbResults=res;
         console.log(omdbResults);
     });
-    //omdbResults={"Title":"Death Note","Year":"2017","Rated":"TV-MA","Released":"25 Aug 2017","Runtime":"101 min","Genre":"Action, Crime, Drama","Director":"Adam Wingard","Writer":"Charley Parlapanides, Vlas Parlapanides, Jeremy Slater","Actors":"Nat Wolff, LaKeith Stanfield, Margaret Qualley","Plot":"A high school student named Light Turner discovers a mysterious notebook that has the power to kill anyone whose name is written within its pages, and launches a secret crusade to rid the world of criminals.","Language":"English, Japanese, Spanish","Country":"United States","Awards":"N/A","Poster":"https://m.media-amazon.com/images/M/MV5BMTUwOTgzMTEyOF5BMl5BanBnXkFtZTgwNTk3MTM5MjI@._V1_SX300.jpg","Ratings":[{"Source":"Internet Movie Database","Value":"4.4/10"},{"Source":"Rotten Tomatoes","Value":"38%"},{"Source":"Metacritic","Value":"43/100"}],"Metascore":"43","imdbRating":"4.4","imdbVotes":"79,564","imdbID":"tt1241317","Type":"movie","DVD":"25 Aug 2017","BoxOffice":"N/A","Production":"N/A","Website":"N/A","Response":"True"};
+    // omdbResults={"Title":"Death Note","Year":"2017","Rated":"TV-MA","Released":"25 Aug 2017","Runtime":"101 min","Genre":"Action, Crime, Drama","Director":"Adam Wingard","Writer":"Charley Parlapanides, Vlas Parlapanides, Jeremy Slater","Actors":"Nat Wolff, LaKeith Stanfield, Margaret Qualley","Plot":"A high school student named Light Turner discovers a mysterious notebook that has the power to kill anyone whose name is written within its pages, and launches a secret crusade to rid the world of criminals.","Language":"English, Japanese, Spanish","Country":"United States","Awards":"N/A","Poster":"https://m.media-amazon.com/images/M/MV5BMTUwOTgzMTEyOF5BMl5BanBnXkFtZTgwNTk3MTM5MjI@._V1_SX300.jpg","Ratings":[{"Source":"Internet Movie Database","Value":"4.4/10"},{"Source":"Rotten Tomatoes","Value":"38%"},{"Source":"Metacritic","Value":"43/100"}],"Metascore":"43","imdbRating":"4.4","imdbVotes":"79,564","imdbID":"tt1241317","Type":"movie","DVD":"25 Aug 2017","BoxOffice":"N/A","Production":"N/A","Website":"N/A","Response":"True"};
 
     //Wait for api results to return before accessing values
     setTimeout(() => {
@@ -28,6 +30,7 @@ if(rawtitle!=null){ //if a title is selected, perform actions
             var imdbVotes=omdbResults.imdbVotes;
             var metaValue;
             var imdbValue;
+            var imdbID=omdbResults.imdbID;
             var rottenValue;
 
             for(i=0; i<ratings.length; i++){
@@ -49,12 +52,27 @@ if(rawtitle!=null){ //if a title is selected, perform actions
         //Create new div to hold IMDB rating content
         var ratingDiv = document.createElement("div");
         ratingDiv.style.class="wrapper";
-        ratingDiv.setAttribute("style", "text-align:center");
-        ratingDiv.setAttribute("style", "float:right");
+        ratingDiv.style.setProperty("text-align", "center");
+        ratingDiv.style.setProperty("float", "right");
+        ratingDiv.style.setProperty("class", "wrapper");
+        ratingDiv.style.setProperty("position", "relative");
+        ratingDiv.id="ratingDiv";
 
-        //Create IMDB large text node
-        var textnodeLarge = document.createElement("span");
-        textnodeLarge.setAttribute("style", "font-size:20px");
+        var textnodeLarge = document.createElement("a");
+        textnodeLarge.href="https://www.imdb.com/title/"+imdbID;
+        textnodeLarge.target="_blank";
+        textnodeLarge.style.setProperty("font-size", "22px");
+        textnodeLarge.addEventListener('mouseover', function(){
+            event.target.style.color = "#CCCCCC";
+        });
+        textnodeLarge.addEventListener('mouseout', function(){
+            event.target.style.color = "#FFFFFF";
+        });
+
+        // textnodeLarge.style.setProperty("text-decoration", "underline");
+        // textnodeLarge.style.setProperty("text-decoration-thickness", "5%");
+
+
 
 
         //Create IMDB small text node
@@ -63,11 +81,19 @@ if(rawtitle!=null){ //if a title is selected, perform actions
         textnodeSmall.style.setProperty("font-style", "italic")
         textnodeSmall.style.setProperty("font-size", "10px")
 
-        //IMDB icon formatting
-        var img = document.createElement("img");
-        img.style.class="icon";
-        img.src = "https://iconarchive.com/download/i80460/uiconstock/socialmedia/IMDb.ico";
-        img.style.width="80px";
+        // //IMDB icon formatting
+        // var img = document.createElement("img");
+        // img.innerHTML='<a href="https://www.imdb.com/?ref_=nv_home">'+img.innerHTML+'</a>';
+        // img.style.class="icon";
+        // img.src = "https://iconarchive.com/download/i80460/uiconstock/socialmedia/IMDb.ico";
+        // img.style.width="80px";
+
+
+        var img = document.createElement("a");
+        img.href="https://www.imdb.com/?ref_=nv_home";
+        img.target="_blank";
+        img.innerHTML='<img class="icon" src="https://iconarchive.com/download/i80460/uiconstock/socialmedia/IMDb.ico" style="width:80px;">';
+
 
         //New div for rating text
         var textDiv = document.createElement("div");
@@ -91,19 +117,21 @@ if(rawtitle!=null){ //if a title is selected, perform actions
             textnodeSmall.innerHTML="</br>"+"("+imdbVotes+" votes)";
         }
 
-        //Append nodes to divs
-        textDiv.appendChild(textnodeLarge);
-        textDiv.appendChild(textnodeSmall);
-        imgDiv.appendChild(img);
-        ratingDiv.appendChild(imgDiv);
-        ratingDiv.appendChild(textDiv);
+        //Append nodes to divs if not on page already
+        if(document.getElementById("ratingDiv")==null){
+            textDiv.appendChild(textnodeLarge);
+            textDiv.appendChild(textnodeSmall);
+            imgDiv.appendChild(img);
+            ratingDiv.appendChild(imgDiv);
+            ratingDiv.appendChild(textDiv);
 
-        //Add main div to right-side info for selected title
-        document.querySelector(".previewModal--detailsMetadata-right").appendChild(ratingDiv);
+            //Add main div to right-side info for selected title
+            document.querySelector(".previewModal--detailsMetadata-right").appendChild(ratingDiv);
 
-        console.log(document.querySelector(".previewModal--detailsMetadata-right"));
-        console.log(document.querySelector(".detail-modal-container").childNodes[1]);
+            console.log(document.querySelector(".previewModal--detailsMetadata-right"));
+            console.log(document.querySelector(".detail-modal-container").childNodes[1]);
+        }
 
-}, 1000);
+    }, 1000);
 
 }
